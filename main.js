2f9c2d245ab8bdd974e3271c0a33c8548941e357
drakkonlord109 <73566859+drakkonlord109@users.noreply.github.com>
2020-10-29 21:32:09 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function cow(){
+ len = document.getElementById("length").value;
+ area = len * len;
+  peri = 4 * len;
+  document.getElementById("vansh").innerHTML = area;
+  document.getElementById("vansh3").innerHTML = peri;
+}
